68f8ba0eb6b7c4ba5f7dd49afcd9ab7a795ba8eb
tatsuya828828 <kkjy011894@gmail.com>
2020-07-11 17:37:00 +0900

    [finish]12-03

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,14 @@ public class Main {
 		// 大まかに捉えればheroもsuperheroもCharacterの一種なので代入することができる
 		Character hero = new Hero("ひろし");
 		Character superHero = new SuperHero("きよし");
+
+		// 外見がCharacter型なのでCharacterで定義していないメソッドを呼び出すことはできない
+		// System.out.println(hero.getName());
+
+		// Characterにも定義してあるメソッドであれば使うことができる
+		// また外側から順に呼び出されるため一番外側で定義されたメソッドが反応する
+		// このような仕組みから継承は、「子クラスは親クラスの一種である」という考え方が重要
+		hero.talk();
+		superHero.talk();
 	}
 }
